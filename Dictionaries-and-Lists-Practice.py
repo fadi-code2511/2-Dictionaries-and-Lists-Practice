9e34a314556bd70b3dd61f5e7ffc0c35cb57161c
Fadi nasr <nasr.fadi2511@gmail.com>
2025-06-23 13:21:12 +0300

    The basic report to print names,grades,avg for each student

--- Dictionaries-and-Lists-Practice.py
+++ Dictionaries-and-Lists-Practice.py
@@ -6,15 +6,44 @@ records = [
 ["Jana", 94], ["Ziad", 75]
 ]
 
-class_journal={}
-for info in records:
+class_journal={}  #creating an empty dictinary.
+for info in records:   # loop for taking the key and value.
     name=info[0]
     grade=info[1]
     if name in class_journal:
-      class_journal[name].append(grade)
+      class_journal[name].append(grade)   # adding the values.
     else:
-        class_journal[name]=[]
-        class_journal[name].append(grade)
+        class_journal[name]=[]       # adding the keys wiht list data type.
+        class_journal[name].append(grade)    #adding the values     .
 
 print (class_journal)
+
+# Part 2: The Basic Report
+# For each student, print:
+# Their name
+# Their list of grades
+# Their average grade (rounded to 2 decimal places)
+
+
+for name in class_journal:  #loop for the names of students.
+  grades=0
+  counter=0
+  print("\n")  # to move to the next line.
+  print(name,end="   ")  # printing the name and keep printing at the same line.
+  print("Grades:",end=" ") # printing the grades and keep printing at the same line.
+  
+  for grade in class_journal[name]:  #loop to find the grades.
+    print(grade,end=" ")
+    grades+=grade
+    counter+=1   # counter to count how many grade we have for each student.
+    avg=grades/counter
+    roundedAvg=round(avg,2)  #using round function to round to 2 decimal places.  (https://www.w3schools.com/python/ref_func_round.asp)
+  print("Average:",roundedAvg,end=" ")
+
+
+
+
+
+
+
 
